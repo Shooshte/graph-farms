6eaf92fa844c190f2f6c89da29947071d8ad05c6
Miha Sustersic <miha.sustersic.work@gmail.com>
2022-01-30 17:49:16 +0100

    additional stying for the shop page

--- apps/front-end/components/loyaltyReward/index.tsx
+++ apps/front-end/components/loyaltyReward/index.tsx
@@ -33,36 +33,41 @@ const LoyaltyReward = ({
   }, [item, discountPercentage]);
 
   return (
-    <div
-      data-testid="loyalty-rewards"
-      className={styles.container}
-      style={{
-        outlineColor: borderColor,
-        outlineWidth: borderSize,
-        outlineOffset: `-${borderSize}`,
-      }}
-    >
+    <div>
       <h1 className="align-center heading-3">Loyalty reward</h1>
-      <div className={styles.imageContainer}>
-        {item && item?.imageURL !== '' ? (
-          <Image
-            alt="introduction"
-            layout="fill"
-            src={item?.imageURL}
-            objectFit="contain"
-            objectPosition="center"
-            quality={100}
-          />
-        ) : null}
-      </div>
-      <h3 className="align-center heading-2 margin-top-1 margin-bottom-4">
-        {item?.name}
-      </h3>
-      <div className={styles.priceContainer}>
-        <h2 className="heading-1">
-          {newPrice.toLocaleString([], { style: 'currency', currency: 'EUR' })}
-        </h2>
-        <p className={styles.pill}>{`-${discountPercentage}%`}</p>
+      <div
+        data-testid="loyalty-rewards"
+        className={styles.container}
+        style={{
+          outlineColor: borderColor,
+          outlineWidth: borderSize,
+          outlineOffset: `-${borderSize}`,
+        }}
+      >
+        <div className={styles.imageContainer}>
+          {item && item?.imageURL !== '' ? (
+            <Image
+              alt="introduction"
+              layout="fill"
+              src={item?.imageURL}
+              objectFit="contain"
+              objectPosition="center"
+              quality={100}
+            />
+          ) : null}
+        </div>
+        <h3 className="align-center heading-2 margin-top-1 margin-bottom-4">
+          {item?.name}
+        </h3>
+        <div className={styles.priceContainer}>
+          <h2 className="heading-1">
+            {newPrice.toLocaleString([], {
+              style: 'currency',
+              currency: 'EUR',
+            })}
+          </h2>
+          <p className={styles.pill}>{`-${discountPercentage}%`}</p>
+        </div>
       </div>
     </div>
   );
--- apps/front-end/components/recommendedItems/index.tsx
+++ apps/front-end/components/recommendedItems/index.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import Image from 'next/image';
-import styles from '../card.module.scss';
+import cardStyles from '../card.module.scss';
+import styles from './recommendedItems.module.scss';
 import { Item, ItemGroup, ItemGroupName } from '../../../../libs/types/index';
 
 export interface RecommendedItemsProps {
@@ -49,38 +50,44 @@ const RecommendedItems = ({
       .slice(0, itemsCount);
   }, [loyaltyItemId, items, itemGroups, itemsCount, shownItemGroups]);
 
-  return shownItems.map((item) => (
-    <div
-      data-testid="recommended-item"
-      className={styles.cardContainer}
-      key={item.id}
-    >
+  return (
+    <div className={styles.container}>
       <h1 className="align-center heading-3">We recommend</h1>
-      <div className={styles.imageContainer}>
-        {includeItemImages && item.imageURL && item.imageURL !== '' ? (
-          <Image
-            alt="introduction"
-            layout="fill"
-            src={item.imageURL}
-            objectFit="contain"
-            objectPosition="center"
-            quality={100}
-          />
-        ) : null}
-      </div>
-      <h3 className="align-center heading-2 margin-top-1 margin-bottom-4">
-        {item.name}
-      </h3>
-      <div className={styles.priceContainer}>
-        <h2 className="heading-1">
-          {item.defaultPrice.toLocaleString([], {
-            style: 'currency',
-            currency: 'EUR',
-          })}
-        </h2>
+      <div className={styles.cardsContainer}>
+        {shownItems.map((item) => (
+          <div
+            data-testid="recommended-item"
+            className={cardStyles.cardContainer}
+            key={item.id}
+          >
+            <div className={cardStyles.imageContainer}>
+              {includeItemImages && item.imageURL && item.imageURL !== '' ? (
+                <Image
+                  alt={`${item.name}`}
+                  layout="fill"
+                  src={item.imageURL}
+                  objectFit="contain"
+                  objectPosition="center"
+                  quality={100}
+                />
+              ) : null}
+            </div>
+            <h3 className="align-center heading-2 margin-top-1 margin-bottom-4">
+              {item.name}
+            </h3>
+            <div className={cardStyles.priceContainer}>
+              <h2 className="heading-1">
+                {item.defaultPrice.toLocaleString([], {
+                  style: 'currency',
+                  currency: 'EUR',
+                })}
+              </h2>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  ));
+  );
 };
 
 export default RecommendedItems;
